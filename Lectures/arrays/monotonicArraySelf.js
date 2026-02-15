2fe5ba8bc9c94dd6d8568b340c1ba766880550bc
nishanth1596 <nishanth1596@gmail.com>
2026-02-15 15:50:10 +0530

    add correct solution

--- Lectures/arrays/monotonicArraySelf.js
+++ Lectures/arrays/monotonicArraySelf.js
@@ -15,28 +15,47 @@
 
 // Solution
 
-const checkMonotonic = function (array) {
-    //write code here to return either true or false 
-    if (array.length <= 1) return true
-    const first = array[0]
-    const last = array[array.length - 1]
+// const checkMonotonic = function (array) {
+//     //write code here to return either true or false 
+//     if (array.length <= 1) return true
+//     const first = array[0]
+//     const last = array[array.length - 1]
 
-    for (let i = 0; i < array.length; i++) {
-        if (first === last) {
-            if (array[i] > array[i + 1] || array[i] < array[i + 1]) return false
-        } else if (first < last){
-            if (array[i] > array[i+1]) return false
-        } else {
-            if (array[i] < array[i+1]) return false
+//     for (let i = 0; i < array.length; i++) {
+//         if (first === last) {
+//             if (array[i] > array[i + 1] || array[i] < array[i + 1]) return false
+//         } else if (first < last){
+//             if (array[i] > array[i+1]) return false
+//         } else {
+//             if (array[i] < array[i+1]) return false
+//         }
+//     }
+//     return true
+// }
+
+function checkMonotonic(array){
+    if (array.length <= 1) return true
+
+    let seenIncrease = false;
+    let seenDecrease = false;
+
+    for (let i =1; i<array.length; i++){
+        if (array[i] > array[i-1]){
+            seenIncrease = true
+        } else if (array[i] < array[i-1]){
+            seenDecrease = true
         }
+
+        if (seenIncrease && seenDecrease) return false
     }
+
     return true
 }
 
-// console.log(checkMonotonic([1, 1, 3, 3, 4, 5]));
-// console.log(checkMonotonic([5, 4, 3, 2, 0, -20]));
-// console.log(checkMonotonic([-20]));
+console.log(checkMonotonic([1, 1, 3, 3, 4, 5]));
+console.log(checkMonotonic([5, 4, 3, 2, 0, -20]));
+console.log(checkMonotonic([-20]));
 console.log(checkMonotonic([1, 2, 2, 3]
 ));
-// console.log(checkMonotonic([1, 2, 3, 4, 5, 6, 7, 1]));
-// console.log(checkMonotonic([3,3,3,2,1]));
+console.log(checkMonotonic([1, 2, 3, 4, 5, 6, 7, 1]));
+console.log(checkMonotonic([3,3,3,2,1]));
