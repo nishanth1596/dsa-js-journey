af31283668c9ddfe5d538e71f9732bb46891314b
nishanth1596 <nishanth1596@gmail.com>
2026-02-18 10:14:20 +0530

    add reverseString optimal solution

---
+++ leetcode_problems/reverseString_344.js
@@ -0,0 +1,33 @@
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s[i] is a printable ascii character.
+
+
+const reverseString = function(s) {
+    for (let i =0; i< s.length/2;i++){
+        const currentLetter = s[i]
+        s[i] = s[s.length - 1 -i]
+        s[s.length-1-i] = currentLetter   
+    }
+
+    return s        
+};
+
+// time = O(n/2) ===> O(n)
+// space = O(1)
+console.log('heya', reverseString(["h","e","l","l","o"]));
