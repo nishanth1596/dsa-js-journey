3b476f04396e9b96afd448e43db2457e48debfd9
nishanth1596 <nishanth1596@gmail.com>
2026-05-10 21:25:23 +0530

    add optimised solution for mergeAlternatly prob

---
+++ leetcode_problems/arrays/easy/mergeAlternately_1768.js
@@ -0,0 +1,60 @@
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+
+ 
+
+// Example 1:
+
+// Input: word1 = "abc", word2 = "pqr"
+// Output: "apbqcr"
+// Explanation: The merged string will be merged as so:
+// word1:  a   b   c
+// word2:    p   q   r
+// merged: a p b q c r
+// Example 2:
+
+// Input: word1 = "ab", word2 = "pqrs"
+// Output: "apbqrs"
+// Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+// word1:  a   b 
+// word2:    p   q   r   s
+// merged: a p b q   r   s
+// Example 3:
+
+// Input: word1 = "abcd", word2 = "pq"
+// Output: "apbqcd"
+// Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+// word1:  a   b   c   d
+// word2:    p   q 
+// merged: a p b q c   d
+ 
+
+// Constraints:
+
+// 1 <= word1.length, word2.length <= 100
+// word1 and word2 consist of lowercase English letters.
+
+function mergeAlternately(word1, word2){
+    let result = ''
+    let first = 0
+    let second = 0
+
+    while (first < word1.length || second < word2.length ){
+        if (word1[first] && word2[second]){
+            result += word1[first]+ word2[second]
+            first++
+            second++
+        } else if (!word1[first] && word2[second]){
+            result += word2[second]
+            second++
+        } else if (!word2[second] && word1[first]){
+            result += word1[first]
+            first++
+        }
+    }
+
+    return result
+}
+
+console.log(mergeAlternately(word1 = "abc", word2 = "pqrsdf"));
